87fcb1ba927db4bfeb17f32ca9e9fcaf94d0ae4b
Martyna Skup <martyna.skup@yahoo.de>
2019-07-19 20:53:43 +0200

    Updates

---
+++ precache-manifest.eb432e584d9e5ee6e35a34d604010373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b5b29203af3c441ca1",
+    "url": "/robofriends/static/css/main.8b8727bc.chunk.css"
+  },
+  {
+    "revision": "f3b5b29203af3c441ca1",
+    "url": "/robofriends/static/js/main.f3b5b292.chunk.js"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "eb9ff28e7e09eb7969f6",
+    "url": "/robofriends/static/css/2.9b66eddf.chunk.css"
+  },
+  {
+    "revision": "eb9ff28e7e09eb7969f6",
+    "url": "/robofriends/static/js/2.eb9ff28e.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "616b4f53f132f8f9298d58d3a2572905",
+    "url": "/robofriends/index.html"
+  }
+];
